53d8a679298127b1795df6947217158dd8d26bb9
dodio <go2solo18@gmail.com>
2015-01-22 00:46:23 +0800

    seo_path默认null

--- install/database.sql
+++ install/database.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS `blog`.`articles`;
+DROP TABLE IF EXISTS `blog`.`articles`;
 CREATE TABLE  `blog`.`articles` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `date_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
@@ -6,9 +7,9 @@ CREATE TABLE  `blog`.`articles` (
   `title` text NOT NULL COMMENT '标题',
   `content` longtext NOT NULL COMMENT '内容',
   `tags` text,
-  `seo_path` varchar(100) NOT NULL COMMENT '文章的url名字',
+  `seo_path` varchar(100) DEFAULT NULL COMMENT '文章的url名字',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `blog`.`tag_article`;
 CREATE TABLE  `blog`.`tag_article` (
